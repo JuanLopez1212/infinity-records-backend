18af12108594df2756ea515526c8b439a95cff12
Juan D Lopez <juandiegolopezortiz4@gmail.com>
2025-05-19 19:31:49 -0500

    [+] index.js: definir 3 rutas para desplegar 3 URLs en el navegador

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-const express = require ( 'express' )
+// const express = require ( 'express' )
+import express from 'express'
 const app = express ( )
+
+app.get ( '/', ( req, res ) => {
+    res.send ( '<h1>Home</h1>')
+})
+app.get ( '/about-us' , ( req, res ) => {
+    res.send ( '<h1>About Us</h1>')
+})
+app.get ( '/contact' , ( req, res ) => {
+    res.send ( '<h1>Contact</h1>')
+})
 
 app.listen ( 3000, () => {
     console.log ( 'Servidor corriendo en http//localhost:3000' )
