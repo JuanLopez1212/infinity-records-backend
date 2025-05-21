0eec3925ae7632647426d02e3ab3b19b5d025fcf
Juan D Lopez <juandiegolopezortiz4@gmail.com>
2025-05-20 19:49:04 -0500

    [+] Se termina la configuración de la base de datos

---
+++ src/config/mongo.config.mjs
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose'
+
+// Define una función asincrona para definir la configuración del ODM Mongoose par usar MongoDB
+async function dbConnect () {
+
+    try {
+        await mongoose.connect ( 'mongodb://localhost:27017/db-infinity-records', {} )
+
+        console.log ( 'Base de datos conectada exitosamente' )
+    }
+    catch (error) {
+        console.error ( error )
+        console.log ( 'Error al conectarse a la base de datos' )
+    }
+
+    // mongoose.connect ( 'mongodb://127.0.0.1:27017/db-infinity-records' )
+    // .then (() => console.log ( 'Conectado'))
+    // .catch ( () => { console.log ( 'Error al conectarse a la base de datos' )})
+}    
+
+export default dbConnect 
--- src/index.mjs
+++ src/index.mjs
@@ -1,10 +1,15 @@
 // const express = require ( 'express' )
 import express from 'express'
 import product from './routes/product.route.mjs' // Importando las rutas de la entidad
+import dbConnect from './config/mongo.config.mjs' // Importando la configuración de la base de datos
 
 
 const app = express ( )
 
+// Invocar la configuración de la conexión a la base de datos	
+dbConnect ()
+
+// Endpoint: http//localhost:3000/
 app.use ( product )
 
 app.listen ( 3000, () => {
