fa7e3bf88b9ff368684304f974613dacd82a7e77
Juan Romero <juan2905sbr@gmail.com>
2025-05-29 19:56:11 -0500

    agregamos auth.router: define post

--- src/routes/auth.router.mjs
+++ src/routes/auth.router.mjs
@@ -1,7 +1,10 @@
 import express from "express";
+import { createUser } from "../controllers/user.controller.mjs";
 
 const router = express.Router();
 
 //define las rutas para la entidad auth
+router.post('/api/login', createUser);
+
 
 export default router;
