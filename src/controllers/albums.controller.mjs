37c22af3083fd34b0b910260ec71a4519d37945e
Juan D Lopez <juandiegolopezortiz4@gmail.com>
2025-06-05 19:21:49 -0500

    Se hacen las rutas y el controlador de albums

--- src/controllers/albums.controller.mjs
+++ src/controllers/albums.controller.mjs
@@ -1,15 +1,93 @@
 import albumsModel from "../schemas/albums.schema.mjs"
 
 const createAlbum = async ( req, res ) => {
-    const inputData = req.body
+    const inputData = req.body        // Extraigo el objeto enviado 
+    
+    // Try: Controla las excepciones de la consulta a la base de datos 
+    try {
+        const registeredAlbums = await albumsModel.create ( inputData )
 
-    const data = await albumsModel.create ( inputData )
-    res.status ( 201 ).json ( data )
+        console.log ( registeredAlbums )  // Imprime en la consola
+        res.send ( registeredAlbums )  // Enviando la respuesta al cliente
+    }
+    catch ( error ) {    // Catch: Captura el error producido por la excepción 
+        console.error ( error )
+        res.status( 500 ).json ( { msg: 'Error al registrar el álbum' } )
+    }
 }
 
+const getAllAlbums = async ( req, res ) => {
+    
+    try {
+        const data = await albumsModel.find ( {} )
+        res.json ( data )     
+    } 
+    catch (error) {
+        console.error ( error )
+        res.json ( { msg: 'Error: No se pudo obtener el listado de álbumes' } )        
+    }
+    
+    
+}
 
+const getAlbumById = async ( req, res ) => {
+    const albumsId = req.params.id    // El nombre final dependerá del nombre del parámetro en la ruta 
+    
+    try {
+        const data = await albumsModel.findById ( albumsId )
+
+        // Verifica si el artista no existe y lanza el respectivo mensaje al cliente
+        if ( ! data ) {
+            return res.json ( { msg: 'El álbum no se encuentra registrado' } )
+        }
+        
+        res.json ( data )
+    } 
+    catch (error) {
+        console.error ( error )
+        res.json ( { msg: 'Error: No se pudo encontrar el álbum' } )
+    }
+}
+
+const removeAlbumsById = async ( req, res ) => {
+    const albumsId = req.params.id
+    
+    try {
+        const data = await artistsModel.findByIdAndDelete ( albumsId )
+        
+        if ( ! data ) {
+        return res.json ( { msg: 'El álbum ya ha sido eliminado' } )
+        }   
+        res.json ( data )    
+    } 
+    catch ( error ) {
+        console.error ( error )
+        res.json ( { msg: 'Error: No se pudo eliminar al álbum' } )
+    }
+    
+    
+}
+
+const updateAlbumById = async ( req, res ) => {
+    const albumId = req.params.id  // Obtenemo el ID de la parametrización de la ruta
+    const inputData = req.body   // Obtenemos el body de la petición
+    
+    try {
+        const data = await albumsModel.findByIdAndUpdate ( albumId, inputData, { new: true } )
+
+        res.json ( data )    
+    } 
+    catch (error) {
+        console.error ( error )
+        res.json ( { msg: 'Error: No se pudo actualizar al álbum' } )
+    }
+}
 
 
 export {
     createAlbum,
+    getAllAlbums,
+    getAlbumById,
+    removeAlbumsById,
+    updateAlbumById
 }
--- src/routes/albums.route.mjs
+++ src/routes/albums.route.mjs
@@ -1,10 +1,15 @@
 import express from 'express' 
-import { createAlbum } from '../controllers/albums.controller.mjs'
+import { createAlbum, getAlbumById, getAllAlbums, removeAlbumsById, updateAlbumById } from '../controllers/albums.controller.mjs'
 
 const router = express.Router ()
 
 // Define las rutas para la entidad "albums"
 router.post ( '/api/albums', createAlbum )
+router.get ( '/api/albums', getAllAlbums)
+router.get ( '/api/albums/:id', getAlbumById )
+router.delete ( '/api/albums/:id', removeAlbumsById )
+router.patch ( '/api/albums/:id', updateAlbumById )
+
 
 
 export default router 
