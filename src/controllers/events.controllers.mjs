eabf9ce6b483465a60cb99abce8eece297e92bba
Juan Romero <juan2905sbr@gmail.com>
2025-07-07 21:44:05 -0500

    se esta creando el fronted events

--- src/controllers/events.controllers.mjs
+++ src/controllers/events.controllers.mjs
@@ -2,16 +2,27 @@ import eventsModel from "../schemas/events.schema.mjs";
 
 const createEvents = async (req, res) => {
     const inputData = req.body;
+    const { role, _id } = req.authUser;
     // CONTROLA LAS EXCEPCIONES DE LA CONSULTA A LA BASE DE DATOS
 
     try{
-    const registeredEvents = await eventsModel.create( inputData); 
 
-    console.log(registeredEvents);  //imprime en la consola
-    res.status(201).json(registeredEvents); //Enviando la respuesta al cliente(
+        if( role !== 'artists' ) {
+            res.json({ error: 'No puedes registrar album' });
+        }
+
+        inputData.userId = _id;
+
+        console.log(req.authuser)
+        const nameArtist = req.authUser
+        inputData.name = nameArtist.name 
+
+        const registeredEvents = await eventsModel.create( inputData); 
+
+        console.log(registeredEvents);  //imprime en la consola
+        res.status(201).json(registeredEvents); //Enviando la respuesta al cliente(
 
     }
-
     catch(error){
         console.error(error);
         res.status( 500 ).json({msg:'Error : No se pudo registar el evento'});
@@ -22,8 +33,8 @@ const createEvents = async (req, res) => {
 const getAllEvents = async (req, res) => {
     
     try{
-    const data = await eventsModel.find({});
-    res.json( data );
+        const data = await eventsModel.find({});
+        res.json( data );
     }
 
     catch(error){
@@ -37,46 +48,59 @@ const getEventsById = async (req, res) => {
 
     try{
     
-        const data = await eventsModel.findById(eventsId);
+        const data = await eventsModel.findById(eventsId).populate(['userId']);
         //  const data = await SongsModel.findOne({_id:SongsId
         //  });
         
-        if(data == null){
-
-            return res.json({msg: 'Error: El evento no se encuentra registrado'});
+        if( ! data ){
+            return res.json({msg: ' El evento no se encuentra registrado'});
         }
 
         res.json(data); 
-
     }
-
-
     catch (error){
         console.error( error);
         res.json({msg: 'Error: No se pudo encontrar el evento'});
     }
 }
 
-const removeEventsById = async (req, res) => {
+const getEventsByArtistId = async ( req, res ) => {
+    const eventsId = req.params.id    // El nombre final dependerá del nombre del parámetro en la ruta 
+    
+    try {
+        const data = await eventsModel.find ({ artistId });
 
+        // Verifica si el artista no existe y lanza el respectivo mensaje al cliente
+        if ( ! data ) {
+            return res.json ( { msg: 'El evento no se encuentra registrado' } )
+        }
+        
+        res.json ( data )
+    } 
+    catch (error) {
+        console.error ( error )
+        res.json ( { msg: 'Error: No se pudo encontrar el evento' } )
+    }
+}
+
+
+
+const removeEventsById = async (req, res) => {
     const eventsId = req.params.id;
 
     try{ 
+        const data = await eventsModel.findByIdAndDelete (eventsId);
 
-    const data = await eventsModel.findByIdAndDelete (eventsId);
-
-    if(data == null){
-
-            return res.json({msg: 'Error: El evento no existe'});
+        if(! data ){
+        return res.json({msg: 'El evento ya ha sido eliminado'});
         }
-
-    res.json(data);
+        res.json(data);
 
     }
 
     catch (error){
         console.error( error);
-        res.json({msg: 'Error: No se pudo encontrar el evento'});
+        res.json({msg: 'Error: No se pudo eliminar el evento'});
     }
 }
 
@@ -103,5 +127,6 @@ export {
     getAllEvents,
     getEventsById, 
     removeEventsById, 
-    updateEventsById
+    updateEventsById,
+    getEventsByArtistId
 }
